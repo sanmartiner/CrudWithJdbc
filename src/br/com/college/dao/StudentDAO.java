97a163de633f09ce2273203e00558ba514d2f697
David San Martin <dsanmartin@outlook.com>
2022-06-10 12:02:43 -0300

    Configure method update and findById of StudentDAO

--- src/br/com/college/dao/StudentDAO.java
+++ src/br/com/college/dao/StudentDAO.java
@@ -39,7 +39,7 @@ public class StudentDAO {
 		}
 	}
 
-	public List<Student> getStudents(){
+	public List<Student> getStudents() {
 
 		String sql = "SELECT * From college.students";
 
@@ -47,19 +47,69 @@ public class StudentDAO {
 
 		Connection conn = null;
 		PreparedStatement pstm = null;
-		
-		//Recovery data on DataBase
+
+		// Recovery data on DataBase
 		ResultSet rset = null;
-		
+
 		try {
 			conn = ConnectionFactory.createConnectionToMySql();
 			pstm = (PreparedStatement) conn.prepareStatement(sql);
 			rset = pstm.executeQuery();
-			
+
 			while (rset.next()) {
-				
-			Student student = new Student();
-			
+
+				Student student = new Student();
+
+				student.setId(rset.getInt(1));
+				student.setName(rset.getString(2));
+				student.setAge(rset.getInt(3));
+				student.setBirthDate(rset.getDate(4));
+				student.setAddress(rset.getString(5));
+				student.setPhone(rset.getString(6));
+				student.setEmail(rset.getString(7));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rset != null) {
+					rset.close();
+				}
+
+				if (pstm != null) {
+					pstm.close();
+				}
+
+				if (conn != null) {
+					conn.close();
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return students;
+
+	}
+
+	public Student getStudentsById(int id) {
+
+		String sql = "SELECT * From college.students WHERE id VALUES(?) ";
+
+		Student student = new Student();
+
+		Connection conn = null;
+		PreparedStatement pstm = null;
+
+		// Recovery data on DataBase
+		ResultSet rset = null;
+
+		try {
+			conn = ConnectionFactory.createConnectionToMySql();
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+			rset = pstm.executeQuery();
+
 			student.setId(rset.getInt(1));
 			student.setName(rset.getString(2));
 			student.setAge(rset.getInt(3));
@@ -67,29 +117,68 @@ public class StudentDAO {
 			student.setAddress(rset.getString(5));
 			student.setPhone(rset.getString(6));
 			student.setEmail(rset.getString(7));
-			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				if(rset != null) {
+				if (rset != null) {
 					rset.close();
 				}
-				
-				if(pstm != null) {
+
+				if (pstm != null) {
 					pstm.close();
 				}
-				
-				if(conn != null) {
+
+				if (conn != null) {
 					conn.close();
 				}
-				
+
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
-		return students;
+
+		return student;
+
+	}
+
+	public void update(Student student) throws SQLException {
+
+		String sql = "UPDATE students SET name = ?, age = ?, birthDate = ?, address = ?, phone = ?, email = ? WHERE id = ?";
+
+		Connection conn = null;
+		PreparedStatement pstm = null;
+
+		try {
+			conn = ConnectionFactory.createConnectionToMySql();
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+
+			pstm.setString(1, student.getName());
+			pstm.setInt(2, student.getAge());
+			pstm.setDate(3, new Date(student.getBirthDate().getTime()));
+			pstm.setString(4, student.getAddress());
+			pstm.setString(5, student.getPhone());
+			pstm.setString(6, student.getEmail());
+
+			pstm.setInt(7, student.getId());
+
+			pstm.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null) {
+				}
+				conn.close();
+
+				if (pstm != null) {
+					pstm.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 
 	}
 
